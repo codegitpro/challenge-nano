515aa474e7134b2107403f2158b09b0fadd6cf39
codegitpro <yusufqing920@gmail.com>
2020-04-04 04:55:59 -0500

    fix: 🐛 fix eslint bug & code style format
    add lodash package, fix coding style using prettier
    
    BREAKING CHANGE: 🧨 package.json, generateData.js, config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,20 @@
-
-const path = require('path');
+const path = require("path");
 
 // import .env variables
-require('dotenv-safe').config({
-  path: path.join(__dirname, '../../.env'),
-  example: path.join(__dirname, '../../.env.example'),
-  allowEmptyValues: true
+require("dotenv-safe").config({
+  path: path.join(__dirname, "../../.env"),
+  example: path.join(__dirname, "../../.env.example"),
+  allowEmptyValues: true,
 });
-
 
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
   mongo: {
-    uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
+    uri:
+      process.env.NODE_ENV === "test"
+        ? process.env.MONGO_URI_TESTS
+        : process.env.MONGO_URI,
   },
 };
--- src/generateData.js
+++ src/generateData.js
@@ -1,33 +1,36 @@
+const logger = require("./config/logger");
 
-const logger = require('./config/logger');
+const Campaign = require("./api/models/campaign.model");
+const Adgroup = require("./api/models/adgroup.model");
+const Ad = require("./api/models/ad.model");
+const Stat = require("./api/models/stat.model");
 
-const Campaign = require('./api/models/campaign.model');
-const Adgroup = require('./api/models/adgroup.model');
-const Ad = require('./api/models/ad.model');
-const Stat = require('./api/models/stat.model');
-
-const data = require('./data');
+const data = require("./data");
 
 const gnerateData = (db) => {
-  db.on('open', async () => {
+  db.on("open", async () => {
     try {
       // Clear existing collections
       await Campaign.deleteMany();
       await Adgroup.deleteMany();
       await Ad.deleteMany();
       await Stat.deleteMany();
-  
+
       // Add data
       for (const campaignData of data.campaigns) {
-        const adgroupsData = data.adgroups.filter(e => e.campaign_id === campaignData.id);
+        const adgroupsData = data.adgroups.filter(
+          (e) => e.campaign_id === campaignData.id
+        );
         const adgroups = [];
-  
+
         for (const adgroupData of adgroupsData) {
-          const adsData = data.ads.filter(e => e.ad_group_id === adgroupData.id);
+          const adsData = data.ads.filter(
+            (e) => e.ad_group_id === adgroupData.id
+          );
           const ads = [];
           for (const adData of adsData) {
-            const statsData = data.stats.filter(e => e.ad_id === adData.id);
-  
+            const statsData = data.stats.filter((e) => e.ad_id === adData.id);
+
             // Create stats
             const stats = [];
             for (const statData of statsData) {
@@ -41,14 +44,14 @@ const gnerateData = (db) => {
             await ad.save();
             ads.push(ad._id);
           }
-  
+
           // Create adgroups
           const adgroup = new Adgroup(adgroupData);
           adgroup.ads = ads;
           await adgroup.save();
           adgroups.push(adgroup._id);
         }
-  
+
         // Create Campaign
         const campaign = new Campaign(campaignData);
         campaign.adgroups = adgroups;
@@ -57,9 +60,9 @@ const gnerateData = (db) => {
     } catch (e) {
       logger.error(e);
     }
-  
-    console.log('data generated...');
-  });  
-}
+
+    console.log("data generated...");
+  });
+};
 
 module.exports = gnerateData;
